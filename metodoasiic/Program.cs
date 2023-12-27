ca97bf46728e346f78f1fbc27ed79e3e0669ce9c
jesusdanielgascaalvrado@gmail.com <jesusdanielgascaalvrado@gmail.com>
2023-12-27 14:36:07 -0600

    Agregar archivos de proyecto.

---
+++ metodoasiic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodoasiic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "ABCDEFGHI";
+            byte[] ASCIIvalues = Encoding.ASCII.GetBytes(str);
+            foreach (var value in ASCIIvalues)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
